e0a44091724cf977127ea21024879825444b42b0
somyaranjan84 <somyaranjan@gmail.com>
2025-07-05 11:08:21 +0530

    Prepare for deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Art Catalog</h1>
+      <p>This is your React app!</p>
+    </div>
+  );
+}
+
+export default App;
